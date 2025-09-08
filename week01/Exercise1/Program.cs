d50b1728141112c4727669cbbe3fe8732d6874b5
Tyler <tyler@Anas-MacBook-Pro.local>
2025-09-07 19:31:56 -0600

    Completed Week 1 - James Bond activity

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?.");
+        
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"The name is {lastName}. {firstName} {lastName}");
+
     }
 }
